2d3fec183041140da830666ff79503ce40d12f7b
FarzanehDabiri71 <farzanehdabiri1371@yahoo.com>
2024-08-04 01:25:00 +0330

    Fix isEditing toggle and update playerNameHandling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Player from "./components/Player";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <h1>React Tic-Tac-Toe</h1>
+      <main>
+        <div id="game-container">
+          {/* players */}
+          <ol id="players">
+            <Player initialName="player 1" symbol={"X"} />
+            <Player initialName="player 2" symbol="O" />
+          </ol>
+          {/* game board */}
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  // State variable to store the player's name, initialized with initialName prop
+  const [playerName, setPlayerName] = useState(initialName);
+
+  // State variable to track whether the name is being edited
+  const [isEditing, setIsEditing] = useState(false);
+
+  // Handler function to toggle the editing mode
+  const handleEditClick = () => {
+    // Toggle the isEditing state between true and false
+    setIsEditing((isEditing) => !isEditing); //true
+    // setIsEditing((isEditing) => !isEditing); // false
+
+    // setIsEditing((!isEditing); //true
+    // setIsEditing( !isEditing); // true
+  };
+
+  // Handler function to update the playerName state when the input value changes
+  const handleChangeName = (e) => {
+    setPlayerName(e.target.value);
+    console.log(playerName);
+    console.log(e.target.value);
+  };
+  // Conditionally set the editablePlayerName to either a span or an input field based on isEditing state
+  let editablePlayerName = <span className="player-name"> {playerName}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChangeName}
+      />
+    );
+
+    btnCaption = "Save";
+  }
+  return (
+    <>
+      <li>
+        <span>
+          {/* player name */}
+          {editablePlayerName}
+          {/* player symbol */}
+          <span className="player-symbol">{symbol}</span>
+        </span>
+        {/* update player name  */}
+        <button onClick={handleEditClick}>{btnCaption}</button>
+        {/* <button onClick={handleEditClick}>{isEditing ? 'Save': 'Edit'}</button> */}
+      </li>
+    </>
+  );
+}
